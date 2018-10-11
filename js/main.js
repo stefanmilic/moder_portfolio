b57304b713a0720424fa2662c9c1941660bdbe13
Stefan Milic <milicstefan6@gmail.com>
2018-10-12 00:27:04 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -1,33 +1,73 @@
-//select elements from the dom
+$(function() {
+  //select elements from the dom
 
-const menuBtn = $(".menu-btn");
-const menu = $(".menu");
-const branding = $(".menu-branding");
-const nav = $(".menu-nav");
-const navItems = $(".nav-item");
+  const menuBtn = $(".menu-btn");
+  const menu = $(".menu");
+  const branding = $(".menu-branding");
+  const nav = $(".menu-nav");
+  const navItems = $(".nav-item");
+  const cross = $(".x");
+  const inputName = $("#name");
+  const textArea = $("#message");
+  const inputEmail = $("#email");
 
-//set initial state of menu
+  //set initial state of menu
 
-let showMenu = false;
+  let showMenu = false;
 
-menuBtn.click(function() {
-  console.log("radi");
-  if (!showMenu) {
-    menuBtn.addClass("close");
-    menu.addClass("show");
-    nav.addClass("show");
-    branding.addClass("show");
-    navItems.addClass("show");
+  menuBtn.click(function() {
+    console.log("radi");
+    if (!showMenu) {
+      menuBtn.addClass("close");
+      menu.addClass("show");
+      nav.addClass("show");
+      branding.addClass("show");
+      navItems.addClass("show");
 
-    //set menu state
-    showMenu = true;
-  } else {
-    menuBtn.removeClass("close");
-    menu.removeClass("show");
-    nav.removeClass("show");
-    branding.removeClass("show");
-    navItems.removeClass("show");
-    //set menu state
-    showMenu = false;
-  }
+      //set menu state
+      showMenu = true;
+    } else {
+      menuBtn.removeClass("close");
+      menu.removeClass("show");
+      nav.removeClass("show");
+      branding.removeClass("show");
+      navItems.removeClass("show");
+      //set menu state
+      showMenu = false;
+    }
+  });
+
+  cross.click(function() {
+    $(".aside").addClass("swing");
+  });
+
+  $("form").on("submit", function(e) {
+    // inputName.val() = "";
+    // textArea.val() = "";
+    // inputEmail.val() = "";
+    var todo = {
+      Name: inputName.val(),
+      Message: textArea.val(),
+      Email: inputEmail.val()
+    }; // prihvata vrednost iz forme
+
+    $.ajax({
+      type: "POST", //triger za post metodu u konteleru
+      url:
+        "https://script.google.com/macros/s/AKfycbzSRkrpenGGdKBfMYkw4Xdzu9pIP-ZZt1zSURwb/exec", // url gde ce to da se izdesava
+      data: todo, //prihvata vresnost polja i smesta u data varijablu i salje da se apdejtuje u kontroler
+      //data je obavezan ne moze da se napise nesto drugo umesto DATA
+      success: function(ovde_moze_bilo_sta) {
+        // obavezan je success i ovde vracamo podatke iz posta u kontrleru
+        //   //do something with the data via front-end framework
+        //reloduje stranicu
+        $("#formMessage").css("display", "inline-block");
+        inputName.val("");
+        textArea.val("");
+        inputEmail.val("");
+      }
+    });
+
+    return false;
+  });
 });
